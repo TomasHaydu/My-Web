de9b912ba0dc37fae2b8d4450f4b6779b9a36a33
Tomas Haydu <tomashaydu@gmail.com>
2023-02-24 21:14:06 -0300

    not fix

--- src/App.jsx
+++ src/App.jsx
@@ -59,7 +59,7 @@ function App() {
   };
 
   return (
-    <div className={dark ? "web-dark" : "web"} id="body">
+    <div className={dark ? "web-dark" : "web"}>
       <nav className="header">
         <div>
           <a href="#home">
